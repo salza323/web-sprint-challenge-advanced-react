3472b337b71a6a70a5c729160ba80f323b176930
salza323 <sal.zamora480@gmail.com>
2020-10-02 10:53:14 -0700

    testing complete

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -42,11 +42,14 @@ const CheckoutForm = (props) => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <h2>Checkout Form</h2>
+        <h2
+        id='headerTitle'
+        >Checkout Form</h2>
         <label>
           First Name:
           <input
             name="firstName"
+            id="firstName"
             value={values.firstName}
             onChange={handleChanges}
           />
@@ -55,6 +58,7 @@ const CheckoutForm = (props) => {
           Last Name:
           <input
             name="lastName"
+            id="lastName"
             value={values.lastName}
             onChange={handleChanges}
           />
@@ -63,21 +67,31 @@ const CheckoutForm = (props) => {
           Address:
           <input
             name="address"
+            id="address"
             value={values.address}
             onChange={handleChanges}
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input 
+          name="city" 
+          id="city" 
+          value={values.city} onChange={handleChanges} />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input 
+          name="state"
+          id="state"
+           value={values.state} onChange={handleChanges} />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input 
+          name="zip"
+          id="zip"
+           value={values.zip} onChange={handleChanges} />
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+describe('tests checkout form', () => {
+    render(<CheckoutForm/>)
 
-test("form header renders", () => {});
+    test("form header renders", () => {
+        render(<CheckoutForm/>)
+    });
 
-test("form shows success message on submit with form details", () => {});
+    test("form shows success message on submit with form details", async() => {
+        render(<CheckoutForm/>)
+
+        //select everything that I will need to submit form
+        const firstNameInput = screen.getByLabelText(/first Name/i)
+        const lastNameInput = screen.getByLabelText(/last Name/i)
+        const adressInput = screen.getByLabelText(/address/i)
+        const cityInput = screen.getByLabelText(/city/i)
+        const stateInput = screen.getByLabelText(/state/i)
+        const zipInput = screen.getByLabelText(/zip/i)
+        const submitButton = screen.getByRole('button', {type: /submit/i});
+
+        //fire events to fill out form and submit
+        fireEvent.change(firstNameInput, {target:{firstName:'Sal'}})
+        fireEvent.change(lastNameInput, {target:{lastName:'Zamora'}})
+        fireEvent.change(adressInput, {target:{adress:'123 House St'}})
+        fireEvent.change(cityInput, {target:{city:'Metropolis'}})
+        fireEvent.change(stateInput, {target:{state:'Narnia'}})
+        fireEvent.change(zipInput, {target:{zip:'90210'}})
+        fireEvent.click(submitButton)
+
+        //when success message is shown, "woo-hoo!" is diplayed, so I will verify that the message is in the window
+        const success = await screen.findByText(/woo-hoo!/i)
+
+    });
+})
