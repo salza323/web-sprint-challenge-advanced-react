6bd5403263149776ad7f1f645d520666835b8999
salza323 <sal.zamora480@gmail.com>
2020-10-02 09:34:07 -0700

    useForm mvp complete

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,27 @@
+
+import React, { useState } from 'react'
+
 // write your custom hook here to control your checkout form
+
+export const useForm = (key, initialValue, cb) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue)
+
+    const handleChanges = e => {
+        // console.log(e.target.name)
+        setValues({
+            ...values, 
+            [e.taget.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        setShowSuccessMessage(true)
+    }
+
+    return [showSuccessMessage, values, handleChanges, handleSubmit]
+
+}
+
+export default useForm
